a321835a8c5e58a48824f36fb1813fa4464878e1
Brendan Neutra <bneutra@users.noreply.github.com>
2023-02-13 13:13:33 -0800

    lint add prompt

--- resize_aurora_cluster.py
+++ resize_aurora_cluster.py
@@ -25,6 +25,7 @@ def parse_args():
     parser.add_argument('--instance-class','-c', help='instance class e.g. db.r5.large')
     parser.add_argument('--region','-r', default='us-east-1', help='aws region')
     parser.add_argument('--dryrun','-d', action='store_true', help='don\'t do anything')
+    parser.add_argument('--skip-prompt','-s', action='store_true', help='don\'t prompt before starting')
     known_args, args = parser.parse_known_args()
     return known_args, args
 
@@ -50,9 +51,8 @@ def get_instance_info(client, db_instance_id):
     instance = instances[0]
     current_class = instance.get('DBInstanceClass')
     # gather utilization stats
-    arn = instance.get('DBInstanceArn')
     status = instance.get('DBInstanceStatus')
-    return arn, current_class, status
+    return current_class, status
 
 
 def wait_for_instance_ready(client, db_instance_id, desired_class):
@@ -64,23 +64,22 @@ def wait_for_instance_ready(client, db_instance_id, desired_class):
     logit('Waiting to start modifying')
     while True:
         time.sleep(sleep_time)
-        arn, size, status = get_instance_info(client, db_instance_id)
+        size, status = get_instance_info(client, db_instance_id)
         logit(f'status is {status}')
         if status == 'modifying':
             break
         if time.time() - start_time_secs > limit_secs:
             raise Exception('Never saw "modifying", ABORT')
 
-    
     # now wait for state 'available'
     start_time_secs = time.time()
-    # size changes take 10m but class changes can take more than 10m
+    # size change can take over 10m
     limit_secs = 1200
     sleep_time = 30
     logit('Waiting to be "available" again')
     while True:
         time.sleep(sleep_time)
-        arn, size, status = get_instance_info(client, db_instance_id)
+        size, status = get_instance_info(client, db_instance_id)
         logit(f'status is {status}')
         if status == 'available' and size == desired_class:
             break
@@ -89,20 +88,28 @@ def wait_for_instance_ready(client, db_instance_id, desired_class):
     return
 
 
+def prompt_user(instances_to_resize, desired_class):
+    print('+' * 40)
+    print(f'Ready to to proceed? Resizing to {desired_class} for:')
+    print(f'{instances_to_resize}\n')
+    input('Press Enter to proceed')
+
+
 def resize_db_cluster(
         client,
-        db_cluster_arn,
+        db_cluster_id,
         desired_class,
-        dryrun):
+        dryrun,
+        skip_prompt):
 
     instance_map = {}
     instance_map['readers'] = []
     response = client.describe_db_clusters(
-                Filters=[
+        Filters=[
             {
                 'Name': 'db-cluster-id',
                 'Values': [
-                    db_cluster_arn,
+                    db_cluster_id,
                 ]
             },
         ]
@@ -119,12 +126,11 @@ def resize_db_cluster(
 
     for member in members:
         dbinstance = member.get('DBInstanceIdentifier')
-        arn, size, status = get_instance_info(client, dbinstance)
+        size, status = get_instance_info(client, dbinstance)
         if status != 'available':
             raise ValueError(f'{dbinstance} status is not "available": {status}')
         db_info = {
             'name': dbinstance,
-            'arn': arn,
             'class': size
         }
         if member.get('IsClusterWriter'):
@@ -135,6 +141,10 @@ def resize_db_cluster(
     # resize readers then writer
     instances_to_resize = instance_map['readers']
     instances_to_resize.append(instance_map['writer'])
+
+    if not skip_prompt and not dryrun:
+        prompt_user(instances_to_resize, desired_class)
+
     for db in instances_to_resize:
         current_class = db['class']
         db_name = db['name']
@@ -168,7 +178,8 @@ def main():
         client,
         db_cluster_id,
         args.instance_class,
-        args.dryrun)
+        args.dryrun,
+        args.skip_prompt)
 
 
 if __name__ == '__main__':
